53bf90a1d912ae8cf51bebd6409e2b11efced5a9
ChaosReminder <88778765+ChaosReminder@users.noreply.github.com>
2022-06-02 23:31:03 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/user");
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("MONGONNECTED === TRUE"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+
+app.listen(5000, () => {
+  console.log("Back RUNNING");
+});
